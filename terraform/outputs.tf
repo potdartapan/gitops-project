dde1f2f052df848ed63daad4bd6cdc80ca477118
Tapan Potdar <potdartapan@gmail.com>
2026-01-22 18:07:22 -0500

    fixed outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,11 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-  description = "The URL needed to push docker images"
-}
-
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
